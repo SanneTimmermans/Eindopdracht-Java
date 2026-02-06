e1b7507475deb024dd199abd0f4f2e09382f4971
SanneTimmermans <s.timmermans03@gmail.com>
2026-02-06 10:49:38 +0100

    Onderdeel laag fundering gelegd zodat deze communiceert met postman

--- src/main/java/nl/projectautoplanner/projectautoplannerwebapi/Repositories/OnderdeelRepository.java
+++ src/main/java/nl/projectautoplanner/projectautoplannerwebapi/Repositories/OnderdeelRepository.java
@@ -1,4 +1,10 @@
 package nl.projectautoplanner.projectautoplannerwebapi.Repositories;
 
-public class OnderdeelRepository {
+import nl.projectautoplanner.projectautoplannerwebapi.DomainModels.Onderdeel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OnderdeelRepository extends JpaRepository<Onderdeel, Long> {
 }
+
